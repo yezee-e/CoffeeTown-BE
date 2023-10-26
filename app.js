cb2aca9b7d33ccfdcaa50d8a1ae0f7d70d635dbb
khj1754 <khj476611@naver.com>
2023-10-26 14:57:34 +0900

    배포CORS해결

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const routers = require('./routers');
 const morgan = require('morgan');
-// const path = require('path');
 
 var corsOptions = {
-  origin: process.env.ALLOW_ORIGIN,
+  origin: '*',
   optionsSuccessStatus: 200,
+  credentials: true,
 };
 
 const createApp = () => {
   const app = express();
   app.use(cors(corsOptions));
-  // app.use(express.static(path.join(__dirname, '../CoffeeTown-FE/build')));
   app.use(express.json());
   
   if(process.env.NODE_ENV == 'production'){
--- routers/categoryRouter.router.js
+++ routers/categoryRouter.router.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const fs = require('fs');
 const categoryController = require('../controllers/category.controller');
-// const imagesController = require('../controllers/images.controller');
 
 router.get('/list', categoryController.list);
 
